2a3a8534e851ede10195e3f3698718ef2a61660c
Gurdip Sira <gurdip.sira@gmail.com>
2022-12-05 13:52:17 +0000

    code

--- build.TempPoint.ps1
+++ build.TempPoint.ps1
@@ -16,13 +16,18 @@
 			
 			if ($Module)
 			{
-			
+				$Version = $Module | Measure-Object -Property Version -Maximum | Select-Object -ExpandProperty Maximum
+				$GalleryVersion = Find-Module -Name $ModuleName -Repository PSGallery | Measure-Object -Property Version -Maximum | Select-Object -ExpandProperty Maximum
+				
+				if ($Version -lt $GalleryVersion)
+				{
 					
 					if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') { Set-PSRepository -Name PSGallery -InstallationPolicy Trusted }
 					
-			
+					Write-Verbose -Message "$($ModuleName) Installed Version [$($Version.tostring())] is outdated. Installing Gallery Version [$($GalleryVersion.tostring())]"
+					
 					Install-Module -Name $ModuleName -Force -SkipPublisherCheck
-					Import-Module -Name $ModuleName -Force -RequiredVersion 
+					Import-Module -Name $ModuleName -Force -RequiredVersion $GalleryVersion
 				}
 				else
 				{
