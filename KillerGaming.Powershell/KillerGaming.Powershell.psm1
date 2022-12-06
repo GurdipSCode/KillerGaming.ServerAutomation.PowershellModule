13bc2b402a6c1a3d164de5d50e641103630d8a0c
Gurdip Sira <gurdip.sira@gmail.com>
2022-12-06 18:34:46 +0000

    code

--- KillerGaming.Powershell/KillerGaming.Powershell.psm1
+++ KillerGaming.Powershell/KillerGaming.Powershell.psm1
@@ -1,36 +1,24 @@
-﻿<#	
-	===========================================================================
-	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2022 v5.8.197
-	 Created on:   	09/01/2022 23:23
-	 Created by:   	Administrator
-	 Organization: 	
-	 Filename:     	KillerGaming.Powershell.psm1
-	-------------------------------------------------------------------------
-	 Module Name: KillerGaming.Powershell
-	===========================================================================
-#>
+﻿    $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+    $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+    $ModuleRoot = $PSScriptRoot
 
-
-#Get public and private function definition files.
-$Public = @(Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue)
-$Private = @(Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue)
+# Import System.Drawing Assembly
+    Try {
+        Add-Type -Assembly System.Drawing
+    }
+    Catch {
+        Write-Error -Message "Failed to import System.Drawing assembly: $_"
+    }
 
 #Dot source the files
-Foreach ($import in @($Public + $Private))
+Foreach($import in @($Public + $Private))
 {
-	Try
-	{
-		. $import.fullname
-	}
-	Catch
-	{
-		Write-Error -Message "Failed to import function $($import.fullname): $_"
-	}
+    Try
+    {
+        . $import.fullname
+    }
+    Catch
+    {
+        Write-Error -Message "Failed to import function $($import.fullname): $_"
+    }
 }
-
-# Here I might...
-# Read in or create an initial config file and variable
-# Export Public functions ($Public.BaseName) for WIP modules
-# Set variables visible to the module and its functions only
-
-Export-ModuleMember -Function $Public.Basename
