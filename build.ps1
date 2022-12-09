73bbc951e96512536192725b8b265ca831735873
Gurdip Sira <gurdip.sira@gmail.com>
2022-12-09 19:08:37 +0000

    s

--- build.ps1
+++ build.ps1
@@ -39,4 +39,38 @@ task GenerateListOfFunctions {
 	Select-String -Path $moduleManifest -Pattern FunctionsToExport
 }
 
-task . GenerateListOfFunctions
+task RunPSScriptAnalyzer {
+
+		try
+		{
+			$outputDIR = [Environment]::GetEnvironmentVariable('KillerGaming.PowershellHyperv Module Output Dir', 'Machine')
+			Write-Host $outputDIR
+			$psscriptAnalyzerDir = Join-Path -Path $outputDIR -ChildPath "PSScriptAnalyzer\psscriptanalyzer.csv"
+			$psscriptAnalyzerHtml = Join-Path -Path $outputDIR -ChildPath "PSScriptAnalyzer"
+			
+			$results = Invoke-ScriptAnalyzer -Path .\KillerGaming.Powershell\Public -Recurse -ErrorAction Stop | Export-Csv $psscriptAnalyzerDir
+			
+			cd C:\Scripts\
+			.\PSScriptAnalyzerReporter.ps1 -OutputPath $psscriptAnalyzerHtml -CsvPath $psscriptAnalyzerDir
+			$results
+		}
+
+		catch
+		{
+			Write-Error -Message $_
+			exit 1
+		}
+		if ($results.Count -gt 0)
+		{
+			Write-Host "Analysis of your code threw $($results.Count) warnings or errors. Please go back and check your code."
+			exit 1
+		}
+		else
+		{
+			Write-Host 'Awesome code! No issues found!' -Foregroundcolor green
+		}
+
+    
+}
+
+task . GenerateListOfFunctions, RunPSScriptAnalyzer
