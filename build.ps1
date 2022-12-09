cc14423c6a33e57e5b7e0fc6386bfb656a56292c
Gurdip Sira <gurdip.sira@gmail.com>
2022-12-09 18:49:18 +0000

    sqsa

--- build.ps1
+++ build.ps1
@@ -1,6 +1,7 @@
 task GenerateListOfFunctions {
     # Set exported functions by finding functions exported by *.psm1 file via Export-ModuleMember
 
+	# Going into module folder
 	cd .\KillerGaming.Powershell
 
     Write-Host "Getting functions..."
@@ -15,7 +16,7 @@ task GenerateListOfFunctions {
 	$moduleName = Get-Item . | ForEach-Object BaseName
 	
 	# RegEx matches files like Verb-Noun.ps1 only, not psakefile.ps1 or *-*.Tests.ps1
-	$functionNames = Get-ChildItem -Path ".\KillerGaming.Powershell\Public" -Recurse | Where-Object { $_.Name -match "^[^\.]+-[^\.]+\.ps1$" } -PipelineVariable file | ForEach-Object {
+	$functionNames = Get-ChildItem -Path ".\Public" -Recurse | Where-Object { $_.Name -match "^[^\.]+-[^\.]+\.ps1$" } -PipelineVariable file | ForEach-Object {
 		$ast = [System.Management.Automation.Language.Parser]::ParseFile($file.FullName, [ref]$null, [ref]$null)
 		if ($ast.EndBlock.Statements.Name)
 		{
@@ -32,5 +33,5 @@ task GenerateListOfFunctions {
 	Select-String -Path .\KillerGaming.Powershell\KillerGaming.Powershell.psd1 -Pattern FunctionsToExport
 }
 
-
+Set-BuildEnvironment
 task . GenerateListOfFunctions
