1181016b4319fcde95f2a0b7c8bb2de4477f9eaf
Gurdip Sira <gurdip.sira@gmail.com>
2022-12-10 00:28:12 +0000

    DS

--- build.ps1
+++ build.ps1
@@ -94,13 +94,12 @@ task RunPSScriptAnalyzer {
 
 task RunPSCodeHealth {
 {
-    try
-    {
+
         $outputDIR = [Environment]::GetEnvironmentVariable('KillerGaming.PowershellHyperv Module Output Dir', 'Machine')
         $psCodeHealth = Join-Path -Path $outputDIR -ChildPath "PSCodeHealth\HealthReport.html"
         
         Invoke-PSCodeHealth -Path .\KillerGaming.Powershell\Public -HtmlReportPath $psCodeHealth
-    }
+    
 }
 }
 
