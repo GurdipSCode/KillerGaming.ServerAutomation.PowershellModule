afc619fb203949e05f0292dede18229275cc8f86
Gurdip Sira <gurdip.sira@gmail.com>
2022-12-10 01:01:57 +0000

    xsa

---
+++ Tests/KillerGaming.Powershell.Tests.ps1
@@ -0,0 +1,30 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2022 v5.8.197
+	 Created on:   	23/02/2022 19:35
+	 Created by:   	Administrator
+	 Organization: 	
+	 Filename:     	KillerGaming.Powershell.Tests.ps1
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+
+
+#Integration test example
+Describe "Get-SEObject PS$PSVersion Integrations tests" {
+	
+	Context 'Strict mode' {
+		
+		Set-StrictMode -Version latest
+		$s = 400
+		
+		It 'should get valid data' {
+		
+			$s -gt 100 | Should be $True
+	
+		}
+	}
+}
