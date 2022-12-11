24df2436042da6e20b30a4487a7b8e2819e134c1
Gurdip Sira <gurdip.sira@gmail.com>
2022-12-11 12:56:50 +0000

    jjki

--- psake.ps1
+++ psake.ps1
@@ -20,7 +20,7 @@ Properties {
     }
 }
 
-Task Default -Depends Init, Test, CheckSyntax
+Task Default -Depends Init, Test, CheckSyntax, GenerateListOfFunctions
 
 Task Init {
     $lines
@@ -78,40 +78,40 @@ $file.FullName | Should Exist
  }
 
 
-# Task GenerateListOfFunctions -Depends CheckSyntax {
+Task GenerateListOfFunctions -Depends CheckSyntax {
     
-#     cd .\KillerGaming.Powershell
+    $modulePath =  Get-Item Env:BHModulePath | select -ExpandProperty Value
+    $moduleManifest = Get-Item Env:BHPSModuleManifest | select -ExpandProperty Value
+    
+    cd .\KillerGaming.Powershell
 
-#     Write-Host "Getting functions..."
+    Write-Host "Getting functions..."
 
-# 	$modulePath = (Get-Item .).FullName
-# 	$moduleManifest = Join-Path -Path $modulePath -ChildPath "/KillerGaming.Powershell.psd1"
+	Write-Host $moduleManifest
 
-# 	Write-Host $moduleManifest
-
-#     Set-Location -Path (Get-Item .).FullName
-# 	Select-String -Path KillerGaming.Powershell.psd1 -Pattern FunctionsToExport
+    Set-Location -Path (Get-Item .).FullName
+	Select-String -Path KillerGaming.Powershell.psd1 -Pattern FunctionsToExport
 	
 
-# 	# Update the psd1 with Set-ModuleFunction:
-# 	$moduleName = Get-Item . | ForEach-Object BaseName
+	# Update the psd1 with Set-ModuleFunction:
+	$moduleName = Get-Item . | ForEach-Object BaseName
 	
-# 	# RegEx matches files like Verb-Noun.ps1 only, not psakefile.ps1 or *-*.Tests.ps1
-# 	$functionNames = Get-ChildItem -Path ".\Public" -Recurse | Where-Object { $_.Name -match "^[^\.]+-[^\.]+\.ps1$" } -PipelineVariable file | ForEach-Object {
-# 		$ast = [System.Management.Automation.Language.Parser]::ParseFile($file.FullName, [ref]$null, [ref]$null)
-# 		if ($ast.EndBlock.Statements.Name)
-# 		{
-# 			$ast.EndBlock.Statements.Name
-# 		}
-# 	}
-# 	Write-Verbose "Using functions $functionNames"
+	# RegEx matches files like Verb-Noun.ps1 only, not psakefile.ps1 or *-*.Tests.ps1
+	$functionNames = Get-ChildItem -Path ".\Public" -Recurse | Where-Object { $_.Name -match "^[^\.]+-[^\.]+\.ps1$" } -PipelineVariable file | ForEach-Object {
+		$ast = [System.Management.Automation.Language.Parser]::ParseFile($file.FullName, [ref]$null, [ref]$null)
+		if ($ast.EndBlock.Statements.Name)
+		{
+			$ast.EndBlock.Statements.Name
+		}
+	}
+	Write-Verbose "Using functions $functionNames"
 	
-# 	Update-ModuleManifest -Path $moduleManifest -FunctionsToExport $functionNames
+	Update-ModuleManifest -Path $moduleManifest -FunctionsToExport $functionNames
 	
-# 	Update-Metadata -Path $moduleManifest
+	Update-Metadata -Path $moduleManifest
 	
-# 	# Check FunctionsToExport again:
-# 	Select-String -Path $moduleManifest -Pattern FunctionsToExport
+	# Check FunctionsToExport again:
+	Select-String -Path $moduleManifest -Pattern FunctionsToExport
 # }
 
 
