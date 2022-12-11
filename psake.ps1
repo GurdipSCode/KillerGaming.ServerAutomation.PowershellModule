c8df1aba2acf6a8b54987ef0c35f9eadac275702
Gurdip Sira <gurdip.sira@gmail.com>
2022-12-11 15:40:22 +0000

    sds

--- psake.ps1
+++ psake.ps1
@@ -121,10 +121,8 @@ Task GenerateListOfFunctions -Depends CheckSyntax {
 
 Task RunPSScriptAnalyzer -Depends GenerateListOfFunctions {
 
-		try
-		{
             $lines 
-            
+
 			$outputDIR = [Environment]::GetEnvironmentVariable('KillerGaming.PowershellHyperv Module Output Dir', 'Machine')
 			Write-Host $outputDIR
 			$psscriptAnalyzerDir = Join-Path -Path $outputDIR -ChildPath "PSScriptAnalyzer\psscriptanalyzer.csv"
@@ -135,22 +133,7 @@ Task RunPSScriptAnalyzer -Depends GenerateListOfFunctions {
 			cd C:\Scripts\
 			.\PSScriptAnalyzerReporter.ps1 -OutputPath $psscriptAnalyzerHtml -CsvPath $psscriptAnalyzerDir
 			$results
-		}
 
-		catch
-		{
-			Write-Error -Message $_
-			exit 1
-		}
-		if ($results.Count -gt 0)
-		{
-			Write-Host "Analysis of your code threw $($results.Count) warnings or errors. Please go back and check your code."
-			exit 1
-		}
-		else
-		{
-			Write-Host 'Awesome code! No issues found!' -Foregroundcolor green
-		}
 }
 
 # Task RunPSCodeHealth -Depends RunPSScriptAnalyzer {
