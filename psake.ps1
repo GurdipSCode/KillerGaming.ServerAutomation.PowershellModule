dd69f4f15a886d6737a2e5682bf17e5d8d4d62c4
Gurdip Sira <gurdip.sira@gmail.com>
2022-12-11 01:43:17 +0000

    dssdds

--- psake.ps1
+++ psake.ps1
@@ -57,215 +57,215 @@ Task Test -Depends Init  {
 }
 
 
-Task CheckSyntax -Depends Test {
+# Task CheckSyntax -Depends Test {
  
-  $scripts = Get-ChildItem -Path .\KillerGaming.Powershell -Include *.ps1, *.psm1, *.psd1 -Recurse |
-  Where-Object {$_.FullName -notmatch 'powershell'}
+#   $scripts = Get-ChildItem -Path .\KillerGaming.Powershell -Include *.ps1, *.psm1, *.psd1 -Recurse |
+#   Where-Object {$_.FullName -notmatch 'powershell'}
 
-# TestCases are splatted to the script so we need hashtables
-$testCases = $scripts | Foreach-Object {@{file = $_}}
+# # TestCases are splatted to the script so we need hashtables
+# $testCases = $scripts | Foreach-Object {@{file = $_}}
 
-It "Script <file> should be valid powershell" -TestCases $testCases {
-param($file)
+# It "Script <file> should be valid powershell" -TestCases $testCases {
+# param($file)
 
-$file.FullName | Should Exist
+# $file.FullName | Should Exist
 
-    $contents = Get-Content -Path $file.FullName -ErrorAction Stop
-    $errors = $null
-    $null = [System.Management.Automation.PSParser]::Tokenize($contents, [ref]$errors)
-    $errors.Count | Should Be 0
-    }
- }
+#     $contents = Get-Content -Path $file.FullName -ErrorAction Stop
+#     $errors = $null
+#     $null = [System.Management.Automation.PSParser]::Tokenize($contents, [ref]$errors)
+#     $errors.Count | Should Be 0
+#     }
+#  }
 
 
-Task GenerateListOfFunctions -Depends CheckSyntax {
+# Task GenerateListOfFunctions -Depends CheckSyntax {
     
-    cd .\KillerGaming.Powershell
+#     cd .\KillerGaming.Powershell
 
-    Write-Host "Getting functions..."
+#     Write-Host "Getting functions..."
 
-	$modulePath = (Get-Item .).FullName
-	$moduleManifest = Join-Path -Path $modulePath -ChildPath "/KillerGaming.Powershell.psd1"
+# 	$modulePath = (Get-Item .).FullName
+# 	$moduleManifest = Join-Path -Path $modulePath -ChildPath "/KillerGaming.Powershell.psd1"
 
-	Write-Host $moduleManifest
+# 	Write-Host $moduleManifest
 
-    Set-Location -Path (Get-Item .).FullName
-	Select-String -Path KillerGaming.Powershell.psd1 -Pattern FunctionsToExport
+#     Set-Location -Path (Get-Item .).FullName
+# 	Select-String -Path KillerGaming.Powershell.psd1 -Pattern FunctionsToExport
 	
 
-	# Update the psd1 with Set-ModuleFunction:
-	$moduleName = Get-Item . | ForEach-Object BaseName
+# 	# Update the psd1 with Set-ModuleFunction:
+# 	$moduleName = Get-Item . | ForEach-Object BaseName
 	
-	# RegEx matches files like Verb-Noun.ps1 only, not psakefile.ps1 or *-*.Tests.ps1
-	$functionNames = Get-ChildItem -Path ".\Public" -Recurse | Where-Object { $_.Name -match "^[^\.]+-[^\.]+\.ps1$" } -PipelineVariable file | ForEach-Object {
-		$ast = [System.Management.Automation.Language.Parser]::ParseFile($file.FullName, [ref]$null, [ref]$null)
-		if ($ast.EndBlock.Statements.Name)
-		{
-			$ast.EndBlock.Statements.Name
-		}
-	}
-	Write-Verbose "Using functions $functionNames"
+# 	# RegEx matches files like Verb-Noun.ps1 only, not psakefile.ps1 or *-*.Tests.ps1
+# 	$functionNames = Get-ChildItem -Path ".\Public" -Recurse | Where-Object { $_.Name -match "^[^\.]+-[^\.]+\.ps1$" } -PipelineVariable file | ForEach-Object {
+# 		$ast = [System.Management.Automation.Language.Parser]::ParseFile($file.FullName, [ref]$null, [ref]$null)
+# 		if ($ast.EndBlock.Statements.Name)
+# 		{
+# 			$ast.EndBlock.Statements.Name
+# 		}
+# 	}
+# 	Write-Verbose "Using functions $functionNames"
 	
-	Update-ModuleManifest -Path $moduleManifest -FunctionsToExport $functionNames
+# 	Update-ModuleManifest -Path $moduleManifest -FunctionsToExport $functionNames
 	
-	Update-Metadata -Path $moduleManifest
+# 	Update-Metadata -Path $moduleManifest
 	
-	# Check FunctionsToExport again:
-	Select-String -Path $moduleManifest -Pattern FunctionsToExport
-}
+# 	# Check FunctionsToExport again:
+# 	Select-String -Path $moduleManifest -Pattern FunctionsToExport
+# }
 
 
-Task RunPSScriptAnalyzer -Depends GenerateListOfFunctions {
+# Task RunPSScriptAnalyzer -Depends GenerateListOfFunctions {
 
-		try
-		{
-			$outputDIR = [Environment]::GetEnvironmentVariable('KillerGaming.PowershellHyperv Module Output Dir', 'Machine')
-			Write-Host $outputDIR
-			$psscriptAnalyzerDir = Join-Path -Path $outputDIR -ChildPath "PSScriptAnalyzer\psscriptanalyzer.csv"
-			$psscriptAnalyzerHtml = Join-Path -Path $outputDIR -ChildPath "PSScriptAnalyzer"
+# 		try
+# 		{
+# 			$outputDIR = [Environment]::GetEnvironmentVariable('KillerGaming.PowershellHyperv Module Output Dir', 'Machine')
+# 			Write-Host $outputDIR
+# 			$psscriptAnalyzerDir = Join-Path -Path $outputDIR -ChildPath "PSScriptAnalyzer\psscriptanalyzer.csv"
+# 			$psscriptAnalyzerHtml = Join-Path -Path $outputDIR -ChildPath "PSScriptAnalyzer"
 			
-			$results = Invoke-ScriptAnalyzer -Path .\KillerGaming.Powershell\Public -Recurse -ErrorAction Stop | Export-Csv $psscriptAnalyzerDir
+# 			$results = Invoke-ScriptAnalyzer -Path .\KillerGaming.Powershell\Public -Recurse -ErrorAction Stop | Export-Csv $psscriptAnalyzerDir
 			
-			cd C:\Scripts\
-			.\PSScriptAnalyzerReporter.ps1 -OutputPath $psscriptAnalyzerHtml -CsvPath $psscriptAnalyzerDir
-			$results
-		}
+# 			cd C:\Scripts\
+# 			.\PSScriptAnalyzerReporter.ps1 -OutputPath $psscriptAnalyzerHtml -CsvPath $psscriptAnalyzerDir
+# 			$results
+# 		}
 
-		catch
-		{
-			Write-Error -Message $_
-			exit 1
-		}
-		if ($results.Count -gt 0)
-		{
-			Write-Host "Analysis of your code threw $($results.Count) warnings or errors. Please go back and check your code."
-			exit 1
-		}
-		else
-		{
-			Write-Host 'Awesome code! No issues found!' -Foregroundcolor green
-		}
-}
+# 		catch
+# 		{
+# 			Write-Error -Message $_
+# 			exit 1
+# 		}
+# 		if ($results.Count -gt 0)
+# 		{
+# 			Write-Host "Analysis of your code threw $($results.Count) warnings or errors. Please go back and check your code."
+# 			exit 1
+# 		}
+# 		else
+# 		{
+# 			Write-Host 'Awesome code! No issues found!' -Foregroundcolor green
+# 		}
+# }
 
-Task RunPSCodeHealth -Depends RunPSScriptAnalyzer {
+# Task RunPSCodeHealth -Depends RunPSScriptAnalyzer {
 
-        $lines 
-        $outputDIR = [Environment]::GetEnvironmentVariable('KillerGaming.PowershellHyperv Module Output Dir', 'Machine')
-        $psCodeHealth = Join-Path -Path $outputDIR -ChildPath "PSCodeHealth\HealthReport.html"
+#         $lines 
+#         $outputDIR = [Environment]::GetEnvironmentVariable('KillerGaming.PowershellHyperv Module Output Dir', 'Machine')
+#         $psCodeHealth = Join-Path -Path $outputDIR -ChildPath "PSCodeHealth\HealthReport.html"
         
-		Write-Host $outputDIR
+# 		Write-Host $outputDIR
    
-		$configuration              = [PesterConfiguration]::Default
-		$configuration.Run.Path     = '.\Tests'
-		$configuration.Run.PassThru = $true
-		$testResult = Invoke-Pester -Configuration $configuration | ConvertTo-Pester4Result
+# 		$configuration              = [PesterConfiguration]::Default
+# 		$configuration.Run.Path     = '.\Tests'
+# 		$configuration.Run.PassThru = $true
+# 		$testResult = Invoke-Pester -Configuration $configuration | ConvertTo-Pester4Result
 
-		Remove-Module Pester -Force
-		Import-Module Pester -MaximumVersion 4.*
+# 		Remove-Module Pester -Force
+# 		Import-Module Pester -MaximumVersion 4.*
 
-		$s = Invoke-PSCodeHealth -Path '.\KillerGaming.Powershell\Public' -TestsResult $testResult -HtmlReportPath $psCodeHealth
+# 		$s = Invoke-PSCodeHealth -Path '.\KillerGaming.Powershell\Public' -TestsResult $testResult -HtmlReportPath $psCodeHealth
 
-		Write-Host $s
+# 		Write-Host $s
 
-Remove-Module Pester -Force
+# Remove-Module Pester -Force
 
-}
+# }
 
-Task SetVersion -Depends RunPSCodeHealth {
+# Task SetVersion -Depends RunPSCodeHealth {
 
-$lines
+# $lines
 
-$outputDIR = [Environment]::GetEnvironmentVariable('KillerGaming.PowershellHyperv Module Output Dir', 'Machine')
-#$outputDIR = "C:\Repos"
-Write-Host $outputDIR
-$fingerprintsDir = Join-Path -Path $outputDIR -ChildPath "fingerprints"
+# $outputDIR = [Environment]::GetEnvironmentVariable('KillerGaming.PowershellHyperv Module Output Dir', 'Machine')
+# #$outputDIR = "C:\Repos"
+# Write-Host $outputDIR
+# $fingerprintsDir = Join-Path -Path $outputDIR -ChildPath "fingerprints"
 
-# Get outgoing module from Proget
-Install-Module -Name "KillerGaming.Powershell" -Repository "KillerGamingPowershell" -Force
-Import-Module -Name "KillerGaming.Powershell" -Force
-$oldCommands = Get-Command -Module "KillerGaming.Powershell"
-Remove-Module -Name "KillerGaming.Powershell"
-
-
-Write-Output 'Calculating fingerprint'
-$oldFingerprint = foreach ( $command in $oldCommands )
-{
-    foreach ( $parameter in $command.parameters.keys )
-    {
-        '{0}:{1}' -f $command.name, $command.parameters[$parameter].Name
-        $command.parameters[$parameter].aliases | 
-            Foreach-Object { '{0}:{1}' -f $command.name, $_}
-    }
-}
-
-Set-Location -Path $fingerprintsDir
-Set-Content -Path .\oldFingerprint -Value $oldFingerprint
-
-$modulePath = "C:\Repos\KillerGaming.Powershell\KillerGaming.Powershell\KillerGaming.Powershell"
-
-# Get new module from src
-Import-Module -Name $modulePath -Force
-$newCommands = Get-Command -Module "KillerGaming.Powershell"
-Remove-Module -Name "KillerGaming.Powershell"
+# # Get outgoing module from Proget
+# Install-Module -Name "KillerGaming.Powershell" -Repository "KillerGamingPowershell" -Force
+# Import-Module -Name "KillerGaming.Powershell" -Force
+# $oldCommands = Get-Command -Module "KillerGaming.Powershell"
+# Remove-Module -Name "KillerGaming.Powershell"
 
 
-Write-Output 'Calculating fingerprint'
-$newFingerprint = foreach ( $command in $newCommands )
-{
-    foreach ( $parameter in $command.parameters.keys )
-    {
-        '{0}:{1}' -f $command.name, $command.parameters[$parameter].Name
-        $command.parameters[$parameter].aliases | 
-            Foreach-Object { '{0}:{1}' -f $command.name, $_}
-    }
-}
+# Write-Output 'Calculating fingerprint'
+# $oldFingerprint = foreach ( $command in $oldCommands )
+# {
+#     foreach ( $parameter in $command.parameters.keys )
+#     {
+#         '{0}:{1}' -f $command.name, $command.parameters[$parameter].Name
+#         $command.parameters[$parameter].aliases | 
+#             Foreach-Object { '{0}:{1}' -f $command.name, $_}
+#     }
+# }
 
-Set-Location -Path $fingerprintsDir
-Set-Content -Path .\newFingerprint -Value $newFingerprint
+# Set-Location -Path $fingerprintsDir
+# Set-Content -Path .\oldFingerprint -Value $oldFingerprint
 
-$bumpVersionType = ''
-$bumpVersionTypeMajor = ''
-$newFingerprint | Where {$_ -notin $oldFingerprint } | 
-    ForEach-Object {$bumpVersionType = 'Minor'; "  $_"}
+# $modulePath = "C:\Repos\KillerGaming.Powershell\KillerGaming.Powershell\KillerGaming.Powershell"
 
-
-$oldFingerprint | Where {$_ -notin $fingerprint } | 
-    ForEach-Object {$bumpVersionTypeMajor = 'Major'; "  $_"}
+# # Get new module from src
+# Import-Module -Name $modulePath -Force
+# $newCommands = Get-Command -Module "KillerGaming.Powershell"
+# Remove-Module -Name "KillerGaming.Powershell"
 
 
-if ($bumpVersionType -eq 'Minor') {
-Step-ModuleVersion -Path $ManifestPath -By $bumpVersionType
-}
+# Write-Output 'Calculating fingerprint'
+# $newFingerprint = foreach ( $command in $newCommands )
+# {
+#     foreach ( $parameter in $command.parameters.keys )
+#     {
+#         '{0}:{1}' -f $command.name, $command.parameters[$parameter].Name
+#         $command.parameters[$parameter].aliases | 
+#             Foreach-Object { '{0}:{1}' -f $command.name, $_}
+#     }
+# }
 
-elseif ($bumpVersionTypeMajor -eq 'Major') {
-Step-ModuleVersion -Path $ManifestPath -By $bumpVersionTypeMajor
-}
+# Set-Location -Path $fingerprintsDir
+# Set-Content -Path .\newFingerprint -Value $newFingerprint
+
+# $bumpVersionType = ''
+# $bumpVersionTypeMajor = ''
+# $newFingerprint | Where {$_ -notin $oldFingerprint } | 
+#     ForEach-Object {$bumpVersionType = 'Minor'; "  $_"}
+
+
+# $oldFingerprint | Where {$_ -notin $fingerprint } | 
+#     ForEach-Object {$bumpVersionTypeMajor = 'Major'; "  $_"}
+
+
+# if ($bumpVersionType -eq 'Minor') {
+# Step-ModuleVersion -Path $ManifestPath -By $bumpVersionType
+# }
+
+# elseif ($bumpVersionTypeMajor -eq 'Major') {
+# Step-ModuleVersion -Path $ManifestPath -By $bumpVersionTypeMajor
+# }
 
 
 
-}
+# }
 
 
-Task CopyModule -Depends SetVersion  {
+# Task CopyModule -Depends SetVersion  {
 
-    $lines
+#     $lines
 
-	$outputDIR = [Environment]::GetEnvironmentVariable('KillerGaming.PowershellHyperv Module Output Dir', 'Machine')
-	$outputModule = Join-Path -Path $outputDIR -ChildPath "KillerGaming.Powershell"
+# 	$outputDIR = [Environment]::GetEnvironmentVariable('KillerGaming.PowershellHyperv Module Output Dir', 'Machine')
+# 	$outputModule = Join-Path -Path $outputDIR -ChildPath "KillerGaming.Powershell"
 			
 
-	$moduleDir = "./KillerGaming.Powershell/*"
+# 	$moduleDir = "./KillerGaming.Powershell/*"
 
-	Copy-Item -Path $moduleDir -Destination $outputModule -Recurse 
-}
-
-
+# 	Copy-Item -Path $moduleDir -Destination $outputModule -Recurse 
+# }
 
 
 
 
-Task PublishModule -Depends CopyModule { 
 
-    $lines
-	Publish-Module -Path E:\output\KillerGaming.PowershellHyperV\717ee3a38b06b58eb361f00aacbae2da54db5dc8\KillerGaming.Powershell -NuGetApiKey a9337a4573920f521dd03092ab173aa4e6184d94 -Repository KillerGamingPowershell2 
-}
+
+# Task PublishModule -Depends CopyModule { 
+
+#     $lines
+# 	Publish-Module -Path E:\output\KillerGaming.PowershellHyperV\717ee3a38b06b58eb361f00aacbae2da54db5dc8\KillerGaming.Powershell -NuGetApiKey a9337a4573920f521dd03092ab173aa4e6184d94 -Repository KillerGamingPowershell2 
+# }
